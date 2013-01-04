e7e1b63819bd599cb1241b835fa6b9fa6c766ab8
griver <griver29@gmail.com>
2013-01-04 19:59:16 +0400

    hello commit

---
+++ src/ru/spbau/mit/ml/blackjack/Main.java
@@ -0,0 +1,14 @@
+package ru.spbau.mit.ml.blackjack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: griver
+ * Date: 04.01.13
+ * Time: 19:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    static public void main(String[] argv) {
+        System.out.println("Hello");
+    }
+}
